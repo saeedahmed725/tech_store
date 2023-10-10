b945b87b72d20db7b6525c810d8ab5aa323e359d
saeedahmed725 <98642572+saeedahmed725@users.noreply.github.com>
2023-10-10 12:27:55 +0300

    finish onboarding screen

--- lib/core/uitls/resources/theme_helper.dart
+++ lib/core/uitls/resources/theme_helper.dart
@@ -1,11 +1,11 @@
 import 'dart:ui';
 
 import 'package:flutter/scheduler.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeHelper {
   Future<bool> getCurrentTheme() async {
-    Brightness brightness = SchedulerBinding.instance.platformDispatcher.platformBrightness;
+    Brightness brightness =
+        SchedulerBinding.instance.platformDispatcher.platformBrightness;
     switch (brightness) {
       case Brightness.dark:
         return true;
